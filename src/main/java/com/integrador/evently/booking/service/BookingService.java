ec9710ed4cc8bdd75bb7d1a4152540dc5028d114
federico bustamante <fede.dhouse@gmail.com>
2023-11-16 19:36:45 -0300

    modified booking service

--- src/main/java/com/integrador/evently/booking/service/BookingService.java
+++ src/main/java/com/integrador/evently/booking/service/BookingService.java
@@ -3,13 +3,16 @@ package com.integrador.evently.booking.service;
 import com.integrador.evently.booking.dto.BookingDTO;
 import com.integrador.evently.booking.model.Booking;
 import com.integrador.evently.booking.repository.BookingRepository;
+import com.integrador.evently.products.dto.ProductDTO;
 import com.integrador.evently.users.model.User;
 import com.integrador.evently.users.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -36,9 +39,56 @@ public class BookingService {
     public Booking createBooking(BookingDTO booking) throws Exception {
         User userOptional = userRepository.findById(booking.getUserId())
                 .orElseThrow(() -> new Exception("User not found"));
+        Optional<List<ProductDTO>> productsInConflict = checkAvailability(booking);
+
+        if(productsInConflict.isPresent()){
+            throw new Exception("The following products are already booked" + productsInConflict);
+        }
+
         Booking bookingEntity = modelMapper.map(booking, Booking.class);
         return bookingRepository.save(bookingEntity);
     }
 
+    public Optional<List<BookingDTO>> getBookingsByDateRange(LocalDateTime startDateTime, LocalDateTime endDateTime){
+        List<BookingDTO> bookings = bookingRepository.findAll().stream()
+                .map(booking -> modelMapper.map(booking, BookingDTO.class)).toList();
+
+        bookings.forEach(booking ->  {
+            if (booking.getEndDateTime().isBefore(startDateTime)){
+                bookings.remove(booking);
+            }else if (booking.getStartDateTime().isAfter(endDateTime)){
+                bookings.remove(booking);
+            };
+        });
+
+        if (!bookings.isEmpty()) {
+            return Optional.of(bookings);
+        }else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<List<ProductDTO>> checkAvailability(BookingDTO booking){
+        Optional<List<BookingDTO>> bookings = getBookingsByDateRange(booking.getStartDateTime(), booking.getEndDateTime());
+
+        if (bookings.isEmpty()){
+            return Optional.empty();
+        }
+
+        List<ProductDTO> productsInConflict = new java.util.ArrayList<>();
+        bookings.get().forEach(existingBooking -> {
+            existingBooking.getProducts().forEach(product -> {
+                    if(booking.getProducts().contains(product)){
+                        productsInConflict.add(product);
+                    }
+            });
+        });
+
+        if (!productsInConflict.isEmpty()) {
+            return Optional.of(productsInConflict);
+        }else {
+            return Optional.empty();
+        }
+    }
 
 }
